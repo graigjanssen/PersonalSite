f7f1c88b318fb0633eb835e3b72868a5abe9af00
Graig Janssen <graig.janssen@gmail.com>
2016-02-17 12:09:01 -0500

    Sets up jquery based navigation, creates and tests web section

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,71 @@
-console.log('loaded...you bastard');
+// VIEWS //
+
+function renderHomeView(){
+  // Hide //
+  $('#web').hide();
+  $('#audio').hide();
+  $('#about').hide();
+  // Show //
+  $('#home').show();
+}
+
+function renderWebView(){
+  // Hide //
+  $('#home').hide();
+  $('#audio').hide();
+  $('#about').hide();
+  // Show //
+  $('#web').show();
+}
+
+function renderAudioView(){
+  // Hide //
+  $('#home').hide();
+  $('#web').hide();
+  $('#about').hide();
+  // Show //
+  $('#audio').show();
+}
+
+function renderAboutView(){
+  // Hide //
+  $('#home').hide();
+  $('#web').hide();
+  $('#audio').hide();
+  // Show //
+  $('#about').show();
+}
+
+// LISTENERS //
+
+function setWebListeners(){
+  $('.to-web').click(function(){
+    renderWebView();
+  });
+}
+
+function setAudioListeners(){
+  $('.to-audio').click(function(){
+    renderAudioView();
+  });
+}
+
+function setAboutListeners() {
+  $('.to-about').click(function(){
+    renderAboutView();
+  });
+}
+
+function setHomeListeners() {
+  $('.to-home').click(function(){
+    renderHomeView();
+  });
+}
+
+$(function(){
+  renderHomeView();
+  setWebListeners();
+  setAudioListeners();
+  setAboutListeners();
+  setHomeListeners();
+});
